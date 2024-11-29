0d12b091b894fb0e21cdb48541c8bc96bac31f66
MarcelFrontend <marceltomasz1806@gmail.com>
2024-11-29 21:48:54 +0100

    test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="overflow-hidden">
       <Head />
       <body className="relative h-screen flex items-center justify-center border antialiased bg-white dark:bg-gray-900 transition-colors duration-[2s] overflow-hidden">
         <Main />
