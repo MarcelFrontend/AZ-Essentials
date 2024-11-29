4768444fc0942d66b38d7435ee1fb4d2268df82d
MarcelFrontend <marceltomasz1806@gmail.com>
2024-11-29 20:55:16 +0100

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -202,7 +202,7 @@ function Index() {
   return (
     // Todo: Jeśli meta viewport nie zadziała przywróć h-[93vh]
     <div className="relative h-[92vh] md:h-screen flex items-center justify-center flex-col gap-16 md:gap-24 lg:gap-32 overflow-hidden">
-      <span className="absolute top-3 left-3 w-96 text-center">
+      <span className="absolute top-3 left-1 w-96 text-center">
         {browserInfo}
       </span>
       <Head>
